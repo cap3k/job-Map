bcf284fff289538e7795a097d8aec7bda5adefbe
Dylan <dylanliner@gmail.com>
2019-12-01 23:21:38 +0100

    feat: no effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { StaticMap } from 'react-map-gl';
-import { PhongMaterial } from '@luma.gl/core';
-import { AmbientLight, PointLight, LightingEffect } from '@deck.gl/core';
 import { HexagonLayer } from '@deck.gl/aggregation-layers';
 import DeckGL from '@deck.gl/react';
 import * as d3 from 'd3';
@@ -10,36 +8,6 @@ const MAPBOX_TOKEN = process.env.REACT_APP_MapboxAccessToken;
 
 const DATA_URL =
   'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv';
-
-const ambientLight = new AmbientLight({
-  color: [255, 255, 255],
-  intensity: 1.0,
-});
-
-const pointLight1 = new PointLight({
-  color: [255, 255, 255],
-  intensity: 0.8,
-  position: [-0.144528, 49.739968, 80000],
-});
-
-const pointLight2 = new PointLight({
-  color: [255, 255, 255],
-  intensity: 0.8,
-  position: [-3.807751, 54.104682, 8000],
-});
-
-const lightingEffect = new LightingEffect({
-  ambientLight,
-  pointLight1,
-  pointLight2,
-});
-
-const material = new PhongMaterial({
-  ambient: 0.64,
-  diffuse: 0.6,
-  shininess: 32,
-  specularColor: [51, 51, 51],
-});
 
 const INITIAL_VIEW_STATE = {
   longitude: -1.4157267858730052,
@@ -91,9 +59,8 @@ const App = () => {
     onHover: (info, event) => {},
     opacity: 1,
     pickable: true,
-    radius: 2000,
+    radius: 1000,
     upperPercentile: 100,
-    material,
 
     transitions: {
       elevationScale: 3000,
@@ -101,12 +68,7 @@ const App = () => {
   });
 
   return (
-    <DeckGL
-      layers={layer}
-      effects={[lightingEffect]}
-      initialViewState={INITIAL_VIEW_STATE}
-      controller
-    >
+    <DeckGL layers={layer} initialViewState={INITIAL_VIEW_STATE} controller>
       <StaticMap
         mapStyle={mapStyle}
         reuseMaps
